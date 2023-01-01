7fdbca677a638325d3e2077580e971019b7f50c2
Pancholi <rakshpan@amazon.com>
2023-01-01 23:31:08 +0530

    new changes

--- chatbot.py
+++ chatbot.py
@@ -56,15 +56,15 @@ def chatbot_response(message):
     res = get_response(ints, intents)
     return res
 
-# from flask import Flask, render_template, request
-# app = Flask(__name__)
-# app.static_folder = 'static'
-# @app.route("/")
-# def home():
-#     return render_template("index.html")
-# @app.route("/get")
-# def get_bot_response():
-#     userText = request.args.get('msg')
-#     return chatbot_response(userText)
-# if __name__ == "__main__":
-#     app.run(debug=True)
+from flask import Flask, render_template, request
+app = Flask(__name__)
+app.static_folder = 'static'
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return chatbot_response(userText)
+if __name__ == "__main__":
+    app.run(debug=True)
